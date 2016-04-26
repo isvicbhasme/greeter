e2de61de3e1ba229325b5994d2d8b738a0588d51
isvicbhasme <isvicbhasme@live.com>
2016-04-26 19:56:02 +0530

    Form validation for login page

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,18 +1,41 @@
 import {Page, NavController} from 'ionic-angular';
 import {ApplyLeavePage} from '../apply-leave/apply-leave';
+import {FORM_DIRECTIVES, AbstractControl, ControlGroup, FormBuilder, Validators} from 'angular2/common';
 
 @Page({
-  templateUrl: 'build/pages/login/login.html'
+  templateUrl: 'build/pages/login/login.html',
+  directives: [FORM_DIRECTIVES] 
 })
 
 export class LoginPage  {
+  minUsernameLen: number;
+  maxUsernameLen: number;
+  minPasswordLen: number;
+  maxPasswordLen: number;
   isLoggedIn: any;
-  constructor(private nav: NavController) {
+  username: AbstractControl;
+  password: AbstractControl;
+  authForm: ControlGroup;
+  
+  constructor(private nav: NavController, fb: FormBuilder) {
     this.isLoggedIn = false;
+    this.minUsernameLen = 4;
+    this.maxUsernameLen = 16;
+    this.minPasswordLen = 6;
+    this.maxPasswordLen = 16;
+    this.authForm = fb.group({
+      'username' : ['', Validators.compose([Validators.required, Validators.minLength(this.minUsernameLen), Validators.maxLength(this.maxUsernameLen)])],
+      'password': ['', Validators.compose([Validators.required, Validators.minLength(this.minPasswordLen), Validators.maxLength(this.maxPasswordLen)])]
+    });
+    this.username = this.authForm.controls['username'];
+    this.password = this.authForm.controls['password'];
   }
   
-  login(username: String, password: String) {
-    this.isLoggedIn = true;
-    this.nav.setRoot(ApplyLeavePage);
+  public login(value: string): void {
+    if(this.authForm.valid) {
+      this.isLoggedIn = true;
+      console.log("Submitted value:"+value);
+      this.nav.setRoot(ApplyLeavePage);
+    }
   }
 }
